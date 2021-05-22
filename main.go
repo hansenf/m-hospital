eb4127f1c928911dc882f9a023af59572f11cea7
miraclemaker17 <hansenferiandy@gmail.com>
2021-05-22 19:49:55 +0700

    rest api  inventory

--- main.go
+++ main.go
@@ -33,16 +33,33 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Endpoint called: homePage()")
 }
 
-//Memanggil http port :8000
+//Function get Inventory
+func getInventory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	fmt.Println("Function Called: getInventory")
+
+	json.NewEncoder(w).Encode(inventory)
+}
+
+//Memanggil page http port :8000
 func handlerRequest() {
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/",homePage).Methods("GET")
+	router.HandleFunc("/", homePage).Methods("GET")
 
+	router.HandleFunc("/inventory",getInventory).Methods("GET")
+	
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 //Halaman Utama
 func main() {
+	inventory = append(inventory, Item{
+		UID: "0",
+		Name: "Cheese",
+		Desc: "A fine block of cheese",
+		Price: 4.99,
+	})
+
 	handlerRequest()
 }
